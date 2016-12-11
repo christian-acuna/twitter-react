b9fbabf115b355a5eb972e714fc80bf50697e74f
Christian Acuña <cacuna0828@gmail.com>
2016-12-10 16:19:38 -0800

    setup env

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,7 @@
-// let's go!
+/*jshint esversion: 6 */
+import React from 'react';
+import { render } from 'react-dom';
+// import App from './Components/App';
+
+
+render(<h1>hi!</h1>, document.getElementById('root'));
