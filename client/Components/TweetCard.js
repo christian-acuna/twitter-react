4b9a2e94a42cde3a8e1cf7afe614f8930dc65405
Christian Acuña <cacuna0828@gmail.com>
2016-12-10 18:59:05 -0800

    add avatar, style avatar, add tooltip for handle

--- client/Components/TweetCard.js
+++ client/Components/TweetCard.js
@@ -1,12 +1,47 @@
 /*jshint esversion: 6 */
 import React from 'react';
-import { Card } from 'antd';
+import { Card, Badge, Tooltip } from 'antd';
+
 
 class TweetCard extends React.Component {
 
-  render() {
+  avatar() {
+    const style = {
+      width: '42px',
+      height: '42px',
+      float: 'right',
+      borderRadius: '6px',
+      background: '#eee',
+      marginTop: '2px',
+
+
+    };
     return (
-      <Card title={this.props.tweet.username} bordered={false}>{this.props.tweet.content}</Card>
+      <div>
+        <Tooltip placement="top" title={this.props.tweet.handle}>
+          <a href="#">{this.props.tweet.username}</a>
+        </Tooltip>
+
+        <img style={style} src={this.props.tweet.avatar_url} alt={this.props.tweet.handle}/>
+      </div>
+    );
+  }
+
+  render() {
+    const style = {
+      paddingRight: '5px',
+      height: '130px'
+    };
+
+    return (
+      <Card
+        title={this.avatar() }
+        style={style}
+        bordered={false}
+        >
+
+        {this.props.tweet.content}
+      </Card>
     );
   }
 
