2e19eab5ec473082acc81692c2bd87301bb43045
Christian Acuña <cacuna0828@gmail.com>
2016-12-10 17:49:14 -0800

    use _.chunk to group cards into rows of three and style

--- client/Components/TweetsContainer.js
+++ client/Components/TweetsContainer.js
@@ -1,42 +1,35 @@
 /*jshint esversion: 6 */
 import React from 'react';
+import { chunk } from 'lodash';
 import { Row, Col, Card } from 'antd';
-import css from './TweetsContainer.css';
+// import css from '../styles/TweetsContainer.css';
+import tweets from '../data/recent_tweets';
 
 class TweetsContainer extends React.Component {
+
+  createTweetArray(tweets) {
+    return tweets.map((tweet, i) => {
+      return (
+        <Col style={{ padding: '8px' }} key={i} span="8">
+          <Card title={tweet.username} bordered={false}>{tweet.content}</Card>
+        </Col>
+      );
+    });
+  }
+
+  createRowsWithTweets(tweets) {
+    const tweetArray = this.createTweetArray(tweets);
+    return chunk(tweetArray, 3).map(function(group, i) {
+      return (
+        <Row style={{ marginLeft: '-8px', marginRight: '-8px'}} key={i}>{group}</Row>
+      );
+    });
+  }
 
   render() {
     return (
       <div style={{ background: '#ECECEC', padding: '30px' }}>
-        <Row>
-          <Col span="8">
-            <Card title="Card title" bordered={false}>Card content</Card>
-          </Col>
-          <Col span="8">
-            <Card title="Card title" bordered={false}>Card content</Card>
-          </Col>
-          <Col span="8">
-            <Card title="Card title" bordered={false}>Card content</Card>
-          </Col>
-        </Row>
-
-
-        {/* <Row type="flex" justify="center">
-          <Col span={4}>
-            <Card title="Card title" >
-              Whatever content
-            </Card>
-          </Col>
-          <Col span={4}><Card title="Card title" >
-            Whatever content
-          </Card></Col>
-          <Col span={4}><Card title="Card title" >
-            Whatever content
-          </Card></Col>
-          <Col span={4}><Card title="Card title" >
-            Whatever content
-          </Card></Col>
-        </Row> */}
+        {this.createRowsWithTweets(tweets)}
       </div>
     );
   }
